c0f426cf2161b6b287e8238572bd17e848e0ab91
Dexter <helpdextron@gmail.com>
2020-12-10 05:56:37 +0100

    fixed a typo

--- aktiver admin og slett temp bruker.bat
+++ aktiver admin og slett temp bruker.bat
@@ -3,7 +3,7 @@
 echo Set password for Admin User
 net user administrator * /active:yes
 
-echo navn paa bruker som skal slettes:
+echo navn pa bruker som skal slettes:
 set /p username=
 net user %username% /delete
 pause
